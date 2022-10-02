e2d2b24b60ea386f051632c3d9ba3e6ca6eb0575
Xavier <48178049+xpalou8@users.noreply.github.com>
2022-10-03 00:40:56 +0200

    Add files via upload

---
+++ register.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="scriptsheet" href="scripts.js">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <title>Register</title>
+</head>
+
+<body>
+
+    <div class="main">
+        <p class="sign" align="center">Register</p>
+        <form action="registrado.php" class="form1" method="get">
+            <input name="nom" class="un " type="text" align="center" placeholder="Nom">
+            <input name="llinatges" class="un " type="text" align="center" placeholder="Llinatges">
+            <input name="nomUsuari" class="un " type="text" align="center" placeholder="Username">
+            <input name="contrassenya" class="pass" type="password" align="center" placeholder="Contrassenya">
+            <button type="submit" class="registerButton">Register</button>
+    </div>
+
+</body>
+
+</html>
